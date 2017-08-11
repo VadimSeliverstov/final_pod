4972fcb35487354f8e860efb7d92efa8ccaeb2d1
Big Fish <big_big_fish@yahoo.com>
2017-08-11 06:33:51 +0300

    Add collection

---
+++ FinalPod/Episode.swift
@@ -0,0 +1,21 @@
+//
+//  Episode.swift
+//  FinalPod
+//
+//  Created by SV-work on 10.08.17.
+//  Copyright © 2017 SV-work. All rights reserved.
+//
+
+import Foundation
+
+struct Episode {
+    var collextionId : Int
+    var episodeId : Int
+    var episodeTitle : String
+    var episodeDescription : String
+    var episodeContentUrl : String
+    var episodeImageUrl : String
+    var episodeReleaseDate : String
+    var episodeSize : Int
+    var episodeDuration : String
+}
